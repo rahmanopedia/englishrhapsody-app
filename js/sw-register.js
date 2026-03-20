7af7dbc292ea08150a2e553048d04f24b811e33f
Rahman Eraydin <rahman.eraydin@gmail.com>
2026-03-20 13:53:21 +0300

    fix: yeni SW devreye girince sayfayı otomatik yenile (controllerchange reload)

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,1 +1,16 @@
-"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("service-worker.js").catch(function(){})});
+if('serviceWorker' in navigator){
+  var _prevCtrl = navigator.serviceWorker.controller;
+  var _reloaded = false;
+
+  // Yeni SW devreye girince sayfayı otomatik yenile
+  navigator.serviceWorker.addEventListener('controllerchange', function(){
+    if(_prevCtrl && !_reloaded){
+      _reloaded = true;
+      window.location.reload();
+    }
+  });
+
+  window.addEventListener('load', function(){
+    navigator.serviceWorker.register('service-worker.js').catch(function(){});
+  });
+}
